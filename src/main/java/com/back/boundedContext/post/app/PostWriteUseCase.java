0eb5291eb28ee4251e259bc95bcff8c860f6cf7c
jj <syeonie0526@gmail.com>
2025-12-23 15:38:57 +0900

    0009

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -9,27 +9,17 @@ import com.back.global.eventPublisher.EventPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
-public class PostService {
+public class PostWriteUseCase {
+
     private final PostRepository postRepository;
-
     private final EventPublisher eventPublisher;
-
-    public long count() {
-        return postRepository.count();
-    }
 
     public void write(Member author, String title, String content) {
         Post post = new Post(author, title, content);
 
         postRepository.save(post);
         eventPublisher.publish(new MemberScoreEvent(author.getId(), ActivityType.POST));
-    }
-
-    public Optional<Post> findById(long id) {
-        return postRepository.findById(id);
     }
 }
