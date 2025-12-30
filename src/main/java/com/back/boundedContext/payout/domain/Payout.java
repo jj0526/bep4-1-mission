7a704d55c036bf0fdca18aad2fb8e9faccebe10f
jj <syeonie0526@gmail.com>
2025-12-30 10:48:16 +0900

    0042 - PayoutMemberCreatedEvent 이벤트 수신 후 Payout 생성

---
+++ src/main/java/com/back/boundedContext/payout/domain/Payout.java
@@ -0,0 +1,54 @@
+package com.back.boundedContext.payout.domain;
+
+import com.back.global.jpa.entity.BaseIdAndTime;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "PAYOUT_PAYOUT")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@SuperBuilder
+public class Payout extends BaseIdAndTime {
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PayoutMember payee;
+    @Setter
+    private LocalDateTime payoutDate;
+    private long amount = 0;
+
+    @OneToMany(mappedBy = "payout", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+    private List<PayoutItem> items = new ArrayList<>();
+
+    public static Payout from(PayoutMember payee) {
+        return Payout.builder()
+                .payee(payee)
+                .build();
+    }
+
+    public PayoutItem addItem(PayoutEventType eventType, String relTypeCode, int relId, LocalDateTime payDate, PayoutMember payer, PayoutMember payee, long amount) {
+        PayoutItem payoutItem = PayoutItem.builder()
+                .payout(this)
+                .eventType(eventType)
+                .relTypeCode(relTypeCode)
+                .relId(relId)
+                .paymentDate(payDate)
+                .payer(payer)
+                .payee(payee)
+                .amount(amount)
+                .build();
+
+        items.add(payoutItem);
+
+        this.amount += amount;
+
+        return payoutItem;
+    }
+}
---
+++ src/main/java/com/back/boundedContext/payout/domain/PayoutItem.java
@@ -0,0 +1,37 @@
+package com.back.boundedContext.payout.domain;
+
+import com.back.global.jpa.entity.BaseIdAndTime;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "PAYOUT_PAYOUT_ITEM")
+@NoArgsConstructor
+@SuperBuilder
+public class PayoutItem extends BaseIdAndTime {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Payout payout;
+
+    @Enumerated(EnumType.STRING)
+    private PayoutEventType eventType;
+
+    String relTypeCode;
+
+    private int relId;
+
+    private LocalDateTime paymentDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PayoutMember payer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PayoutMember payee;
+
+    private long amount;
+
+
+}
